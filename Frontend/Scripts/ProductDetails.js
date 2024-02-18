c9d9d3bd5eeb34a97055f15c5f47f6ad4e856669
trumpmcdonaldz <thetrumpmcdonaldz@gmail.com>
2024-02-18 17:24:30 +0800

    Better onLoad() handling for ProductDetails.js

--- Frontend/Scripts/ProductDetails.js
+++ Frontend/Scripts/ProductDetails.js
@@ -1,6 +1,7 @@
 import { Product } from "../../Common/DataStructures.js";
 import { PRODUCTS_ROUTE_NAME, PRODUCT_PAGE_ROUTE, CATEGORY_ID, JSON_HEADER } from "/Common/Constants.js";
 import { populateCategorySelector, constructProductFromDocument } from "./Shared.js";
+import { isNullOrWhitespace } from "../../Common/Helpers.js";
 
 const FORM_ID = "product_details_form",
       BACK_BUTTON_ID = "back_button";
@@ -13,58 +14,65 @@ async function onLoad()
 
     let productID = queries.get("ID");
 
-    if (productID == null)
+    while (true)
     {
-        // If there's no ID, redirect back to Product.html
-        location.href = PRODUCT_PAGE_URL
+        if (isNullOrWhitespace(productID))
+        {
+            // If there's no ID, redirect back to Product.html
+            break;
+        }
+
+        document.getElementById(FORM_ID).addEventListener("submit", function (event)
+        {
+            onSubmit(event, productID);
+        });
+
+        let response = await fetch(`${PRODUCTS_ROUTE_NAME}/${productID}`);
+
+        let responseText = await response.text();
+
+        if (responseText === "{}")
+        {
+            // If the response body is an empty object, redirect back to Product.html as well
+            break;
+        }
+
+        let product = new Product(JSON.parse(responseText));
+
+        for (let [fieldName, value] of Object.entries(product))
+        {
+            if (value === undefined)
+            {
+                continue;
+            }
+
+            const element = document.getElementById(fieldName);
+
+            if (element == null)
+            {
+                continue;
+            }
+
+            switch (fieldName)
+            {
+                case CATEGORY_ID:
+                    await populateCategorySelector(element);
+                    break;
+            }
+
+            element.value = value;
+        }
+
+        document.getElementById(BACK_BUTTON_ID).addEventListener("click", function (_)
+        {
+            location.href = `/Product.html?ID=${productID}`;
+        });
+
         return;
     }
 
-    document.getElementById(FORM_ID).addEventListener("submit", function (event)
-    {
-        onSubmit(event, productID);
-    });
-
-    let response = await fetch(`${PRODUCTS_ROUTE_NAME}/${productID}`);
-
-    let responseText = await response.text();
-
-    if (responseText === "{}")
-    {
-        location.href = PRODUCT_PAGE_URL;
-        return;
-    }
-
-    let product = new Product(JSON.parse(responseText));
-
-    for (let [fieldName, value] of Object.entries(product))
-    {
-        if (value === undefined)
-        {
-            continue;
-        }
-
-        const element = document.getElementById(fieldName);
-
-        if (element == null)
-        {
-            continue;
-        }
-
-        switch (fieldName)
-        {
-            case CATEGORY_ID:
-                await populateCategorySelector(element);
-                break;
-        }
-
-        element.value = value;
-    }
-
-    document.getElementById(BACK_BUTTON_ID).addEventListener("click", function (_)
-    {
-        location.href = `/Product.html?ID=${productID}`;
-    });
+    // If there's no ID, redirect back to Product.html
+    location.href = PRODUCT_PAGE_ROUTE;
 }
 
 async function onSubmit(event, productID)
